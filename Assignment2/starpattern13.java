b975e06ef1a569beae2f9315f0d32af3b7f44a7d
Hitakshi Chaudhary <90826737+HitakshiC@users.noreply.github.com>
2023-03-18 23:35:28 +0530

    Add files via upload

---
+++ Assignment2/starpattern13.java
@@ -0,0 +1,32 @@
+class starpattern13{
+	public static void main(String args[]){
+		int n=5;
+		//upper pattern
+		for(int i=n;i>0;i--){
+			for(int j=1;j<=n-i;j++){
+				System.out.print(" ");
+			}
+			
+			for(int j=1;j<=i;j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		//lower pattern
+		for(int i=1;i<n;i++){
+			for(int j=1;j<=n-i-1;j++){
+				System.out.print(" ");
+			}
+			
+			for(int j=1;j<=i+1;j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
+		
+
+
+	}
+}
